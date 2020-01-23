69075bc153d0af59def84e18a1bb60da1bd6357c
olginger <olgbelous@gmail.com>
2020-01-23 15:43:34 +0300

    refactoring

--- app/src/main/java/com/example/tribune_app/MainActivity.kt
+++ app/src/main/java/com/example/tribune_app/MainActivity.kt
@@ -19,6 +19,16 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        authenticate()
+
+        btn_registration.setOnClickListener {
+            val registrationIntent = Intent(this@MainActivity, RegistrationActivity::class.java)
+            startActivity(registrationIntent)
+        }
+    }
+
+    private fun authenticate() {
         if (isAuthenticated()) {
             val token = getSharedPreferences(
                 API_SHARED_FILE,
@@ -27,9 +37,7 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
                 .getString(AUTHENTICATED_SHARED_KEY, "")
             Repository.createRetrofitWithAuth(requireNotNull(token))
 
-            val feedActivityIntent = Intent(this@MainActivity, FeedActivity::class.java)
-            startActivity(feedActivityIntent)
-            finish()
+            startFeedActivity()
         } else {
             btn_login.setOnClickListener {
                 launch {
@@ -50,22 +58,20 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
                         toast(R.string.success)
                         setUserAuth(requireNotNull(response.body()).token)
                         Repository.createRetrofitWithAuth(requireNotNull(response.body()).token)
-                        val feedActivityIntent =
-                            Intent(this@MainActivity, FeedActivity::class.java)
-                        startActivity(feedActivityIntent)
-                        finish()
+
+                        startFeedActivity()
                     } else {
                         toast(R.string.authentication_failed)
                     }
                 }
             }
         }
+    }
 
-        btn_registration.setOnClickListener {
-            val registrationIntent = Intent(this@MainActivity, RegistrationActivity::class.java)
-            startActivity(registrationIntent)
-        }
-
+    private fun startFeedActivity(){
+        val feedActivityIntent = Intent(this@MainActivity, FeedActivity::class.java)
+        startActivity(feedActivityIntent)
+        finish()
     }
 
     override fun onStart() {
